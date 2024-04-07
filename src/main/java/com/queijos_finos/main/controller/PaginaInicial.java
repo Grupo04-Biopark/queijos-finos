ccae0af2440aa04868be4f592ec6a8fbf017f0b9
arthurritzel <ritzelarthur@gmail.com>
2024-04-07 19:08:12 -0300

    validacao final

---
+++ src/main/java/com/queijos_finos/main/controller/PaginaInicial.java
@@ -0,0 +1,35 @@
+package com.queijos_finos.main.controller;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.queijos_finos.main.model.Usuarios;
+import com.queijos_finos.main.repository.UsuarioRepository;
+
+@Controller
+public class PaginaInicial {
+	@Autowired
+	private UsuarioRepository usuarioRepo;
+	
+	@GetMapping("/paginaInicial")
+	public String login(Model model) {
+	
+		
+		Usuarios usu = usuarioRepo.findByEmail("ritzelarthur");
+		
+		System.out.println(usu.getNome());
+		model.addAttribute("mensagem", "deu certo ");
+		return "paginaInicial";
+		
+	}
+	
+	
+	
+}
--- src/main/java/com/queijos_finos/main/controller/UsuarioController.java
+++ src/main/java/com/queijos_finos/main/controller/UsuarioController.java
@@ -3,24 +3,33 @@ package com.queijos_finos.main.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.queijos_finos.main.model.Usuarios;
 import com.queijos_finos.main.repository.UsuarioRepository;
 
 
-@RestController
-@RequestMapping("/usuarios")
+@Controller
 public class UsuarioController {
 	
 	@Autowired
 	private UsuarioRepository usuarioRepo;
 	
+	@GetMapping("/")
+	public ModelAndView login() {
+		ModelAndView modelview = new ModelAndView();
+		modelview.setViewName("login");
+		return modelview;
+	}
+	
+	
 	@PostMapping("/login")
-	public void login(@RequestParam("email") String email,
+	public String login(@RequestParam("email") String email,
             					@RequestParam("senha") String senha,
             					Model model) {
 		
@@ -30,10 +39,14 @@ public class UsuarioController {
 		System.out.println(usu.getSenha());
 		
 		if(usu.getSenha().equals(senha)) {
-			System.out.println("Ta certo rapaiz");
+			return "redirect:/paginaInicial";
 		}else {
-			System.out.println("Se fudeu");
+			
+			model.addAttribute("mensagem", "Credenciais invalidas");
+			return "login";
 		}
+		
+		
 		
 	}
 }
