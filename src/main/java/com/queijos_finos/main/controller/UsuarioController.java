ce6f07cdc8eb3018b6bfa2d49b50f4f3e3c2be74
Nathan Breier <135669952+Nbreier26@users.noreply.github.com>
2024-04-25 22:48:11 -0300

    Usuarios
    Criação do controller e html para visualização da lista de usuários

--- src/main/java/com/queijos_finos/main/controller/UsuarioController.java
+++ src/main/java/com/queijos_finos/main/controller/UsuarioController.java
@@ -1,14 +1,22 @@
 package com.queijos_finos.main.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+
 import com.queijos_finos.main.model.Usuarios;
 import com.queijos_finos.main.repository.UsuarioRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 
 @Controller
@@ -16,7 +24,68 @@ public class UsuarioController {
 	
 	@Autowired
 	private UsuarioRepository usuarioRepo;
-	
+
+
+	@GetMapping("/usuarios")
+	public String showUsuarios(Model model) {
+		Usuarios usuarios = new Usuarios();
+		Pageable pageable = PageRequest.of(0, 10);
+		List<Usuarios> usuario = usuarioRepo.findAll(pageable).getContent();
+		model.addAttribute("usuarios", usuario);
+		return "usuarios";
+	}
+
+	@PostMapping("/usuarios")
+	public String createUsuario(@RequestParam("id") Long id,
+								@RequestParam("nome") String nome,
+								@RequestParam("email") String email,
+								@RequestParam("senha") String senha,
+								Model model) {
+
+		if (id != -1) {
+			usuarioRepo.findById(id)
+					.map(usuarios -> {
+						usuarios.setNome(nome);
+						usuarios.setEmail(email);
+						usuarios.setSenha(senha);
+						return usuarioRepo.save(usuarios);
+					})
+					.orElseThrow(() -> new RuntimeException("Usuário não encontrado com o ID: " + id));
+		} else {
+			Usuarios usuarios = new Usuarios();
+			usuarios.setNome(nome);
+			usuarios.setEmail(email);
+
+			usuarios.setSenha(senha);
+			usuarioRepo.save(usuarios);
+		}
+
+		model.addAttribute("mensagem", "Usuário salvo com sucesso");
+		return "redirect:/usuarios";
+	}
+
+	@PostMapping("/usuario/delete/{id}")
+	public String deleteUsuario(@PathVariable("id") Long id,
+								Model model) {
+
+		usuarioRepo.deleteById(id);
+		return "redirect:/usuarios";
+	}
+
+	@GetMapping("/usuarios/cadastrar")
+	public String createUsuarioView(@RequestParam(required = false) Long idUsuario,
+									Model model) {
+
+		if (idUsuario != null) {
+			Optional<Usuarios> usuarios = usuarioRepo.findById(idUsuario);
+			model.addAttribute("usuario", usuarios.orElse(null));
+		}
+
+		return "usuariosCadastrar";
+	}
+
+
+
 	@GetMapping("/")
 	public ModelAndView login() {
 		ModelAndView modelview = new ModelAndView();
@@ -43,7 +112,8 @@ public class UsuarioController {
 			return "login";
 		}
 		
-		
-		
 	}
+	
+	
+	
 }
--- src/main/java/com/queijos_finos/main/repository/UsuarioRepository.java
+++ src/main/java/com/queijos_finos/main/repository/UsuarioRepository.java
@@ -1,7 +1,7 @@
 package com.queijos_finos.main.repository;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -10,5 +10,6 @@ import com.queijos_finos.main.model.Usuarios;
 
 public interface UsuarioRepository extends JpaRepository<Usuarios, Long>{
 	@Query("select u from Usuarios u where u.email=:email")
-	Usuarios findByEmail(String email); 
+	Usuarios findByEmail(String email);
+	Page<Usuarios> findAll(Pageable pageable);
 }
